b0c6833dd7e733b979247f901ad91c810be6e1de
Victor <33927289+DADUSHKA@users.noreply.github.com>
2023-09-27 12:54:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,121 +1,146 @@
-const message = 'Table'
+const FORM = document.querySelector("form");
 
-document.querySelector('#header').innerHTML = message
+const PopupWindow = () => {
+  const mypopup = document.getElementById("mypopup");
+  const style = getComputedStyle(mypopup);
+  if (style.display != "none") {
+    mypopup.style.display = "none";
+  } else {
+    mypopup.style.display = "block";
+    FORM.addEventListener("submit", handleFormData);
+    FORM.addEventListener("click", event => clearInputField(event))
+    changeFile();
+  }
+}
 
-function loadData(url, onSuccess, onError) {
-  const Http = new XMLHttpRequest()
-  Http.open('GET', url, true)
+const changeFile = () => {
+  const uploadButton = document.getElementById("upload-button");
+  const fileInput = document.querySelector("input[type=file]");
+  const avatar = document.getElementById("avatar");
+  const removeButton = document.getElementById('remove-image-button');
 
-  Http.onload = function () {
-    if (Http.status === 200) {
-      const data = JSON.parse(Http.responseText)
-      onSuccess(data)
+  uploadButton.addEventListener("click", () => fileInput.click());
+
+  fileInput.addEventListener("change", event => {
+    clearErrorText();
+
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        avatar.setAttribute("aria-label", file.name);
+        avatar.style.background = `url(${reader.result}) center center/cover`;
+        avatar.classList.remove('image');
+        uploadButton.style.display = "none";
+      };
+      removeButton.disabled = false;
     } else {
-      onError(Http.statusText)
+      avatar.style.background = '';
+      removeButton.disabled = true;
     }
-  };
+  });
 
-  Http.onerror = function () {
-    onError('Network error')
-  }
-
-  Http.send()
-}
-
-function loadTable(data) {
-  const tableBody = document.querySelector('tbody')
-  tableBody.innerHTML = ''
-
-  // data.slice(0, 5).forEach((row) => {
-  data.forEach((row) => {
-    const tr = document.createElement('tr')
-    tableBody.appendChild(tr)
-
-    Object.values(row).forEach((value) => {
-      const td = document.createElement('td')
-      td.textContent = value
-      tr.appendChild(td)
-    })
+  removeButton.addEventListener('click', event => {
+    uploadButton.style.display = "block"
+    avatar.classList.add('image');
+    fileInput.value = '';
+    avatar.style.background = '';
+    removeButton.disabled = true;
   })
+}
 
-  let table = document.getElementById('table')
+const showError = (field, errorText) => {
+  field.classList.add("error");
 
-  table.onclick = function (e) {
-    if (e.target.tagName != 'TH') return
-    let th = e.target
-    let sortDirection =
-      header.getAttribute("data-sort-direction") === "asc" ? "desc" : "asc"
-    header.setAttribute("data-sort-direction", sortDirection)
+  const errorElement = document.createElement("small");
+  errorElement.classList.add("error-text");
+  errorElement.innerText = errorText;
 
-    sortTable(th.cellIndex, th.dataset.type, table, sortDirection)
+  field.closest(".form-group").appendChild(errorElement);
+}
+
+const handleFormData = (e) => {
+  if (e.which == 13) {
+    alert(e.target.id + '---' + e.target.value);
+  }
+
+  e.preventDefault();
+
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const telephoneInput = document.getElementById("telephone");
+  const orientationInput = document.getElementById("orientation");
+  const chiefInput = document.getElementById("chief");
+  const avatar = document.getElementById("avatar");
+
+  const nameLabel = document.getElementById("nameLabel").innerHTML;
+  const telephoneLabel = document.getElementById("telephoneLabel").innerHTML;
+  const emailLabel = document.getElementById("emailLabel").innerHTML;
+  const chiefLabel = document.getElementById("chiefLabel").innerHTML;
+
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const telephone = telephoneInput.value;
+  const orientation = orientationInput.value;
+  const chief = chiefInput.value;
+
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  const numberPattern = /^[0-9]+$/;
+
+  document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
+  document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
+
+  if (avatar.style.background === "" ) {
+    showError(avatar, "Сделайте выбор логотипа");
+  }
+  if (name === "" || nameLabel !== "") {
+    showError(nameInput, "Введите название организации");
+  }
+  if (!emailPattern.test(email) || emailLabel !== "") {
+    showError(emailInput, "Введите действительный адрес электронной почты");
+  }
+  if (!numberPattern.test(telephone) || telephone === "" || telephoneLabel !== "") {
+    showError(telephoneInput, "Введите свой номер телефона");
+  }
+  if (orientation === "" ) {
+    showError(orientationInput, "Выберите нужное направление деятельности");
+  }
+  if (chief === "" || chiefLabel !== "") {
+    showError(chiefInput, "Введите руководителя");
+  }
+
+  const errorInputs = document.querySelectorAll(".form-group .error");
+  if (errorInputs.length > 0) return;
+
+  FORM.submit();
+}
+
+const clearInputField = (e) => {
+  clearErrorText();
+
+  const values = ["name", "telephone", "email",  "chief", "avcInput", "facebookInput", "okInput", "instaInput", "youtubeInput", "vkInput"];
+  const id = e.target.id;
+
+  if (values.indexOf(id) != -1) {
+    const idInput = e.target.id;
+    const nameInput = document.getElementById(idInput);
+    nameInput.value = "";
+
+    const nameLabel = document.getElementById(idInput + 'Label');
+    const value = nameLabel !== null ? nameLabel.innerText : "Найти в соцсети";
+
+    nameInput.placeholder = value;
+    if (nameLabel !== null) nameLabel.innerHTML = "";
   }
 }
 
-function showError(message) {
-  const error = document.createElement('div')
-  error.className = 'error'
-  error.textContent = message
-  document.body.insertBefore(error, document.body.firstChild)
+const clearErrorText = () => {
+  const documentErrors = document.getElementsByClassName("error-text");
+  const arrayErrors = [...documentErrors];
+
+  arrayErrors.forEach(function (element) {
+    element.innerText = "";
+  });
 }
-
-function sortTable(colNum, type, table, direction) {
-  let tbody = table.querySelector('tbody')
-  let rowsArray = Array.from(tbody.rows)
-  let compare;
-
-  switch(type) {
-    case 'number':
-      compare = function(rowA, rowB) {
-        let result;
-
-        if (direction == 'asc') {
-          result = rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML
-        } else {
-          result = rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
-        }
-
-        return result
-      }
-      break;
-
-    case 'string':
-      compare = function (rowA, rowB) {
-        let result;
-
-        if (direction == 'asc') {
-           result = rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML
-        } else {
-           result = rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML
-        }
-
-        return result ? 1 : -1
-      }
-      break;
-  }
-
-  rowsArray.sort(compare)
-
-  tbody.append(...rowsArray)
-}
-
-function tableSearch() {
-  let phrase = document.getElementById('input').value
-  let table = document.getElementById('table')
-
-  let regPhrase = new RegExp(phrase.substring(0, phrase.length - 1), 'i')
-    let flag = false
-    for (let i = 1; i < table.rows.length; i++) {
-      flag = false
-      for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
-        flag = regPhrase.test(table.rows[i].cells[j].innerHTML)
-        if (flag) break
-      }
-      if (flag) {
-        table.rows[i].style.display = ""
-      } else {
-        table.rows[i].style.display = "none"
-      }
-    }
-}
-
-loadData('https://jsonplaceholder.typicode.com/posts', loadTable, showError);
